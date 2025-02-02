8413a48a42b376225ab172c62d160bb388cb2881
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2025-02-02 13:44:21 -0800

    rewrite it all because i made it badly

--- pascalTriangle.java
+++ pascalTriangle.java
@@ -5,12 +5,9 @@ public class pascalTriangle {
 
     public static void main(String[] args){
         List<List<Integer>> pascalsssss = generate(3);
-        for (List<Integer> pascals : pascalsssss) {
-            for (Integer pascal : pascals) {
-                System.out.print(pascal + " ");
-            }
-            System.out.println();
-        }
+        
+            System.out.println(pascalsssss);
+
     }
     public static List<List<Integer>> generate(int numRows) {
         // given num rows, make list of size num rows
@@ -19,20 +16,17 @@ public class pascalTriangle {
         // if position (length) = fill 1,
         // else, fill with ((List(current_row - 1)(position - 1)) - (List(current_row - 1)(position - 1)))
 
-        List<List<Integer>> triangle = new ArrayList<>();
-        List<Integer> currentRow, previousRow;
-        for (int i = 1; i < numRows; i++) {
-            triangle.add(new ArrayList<>(i));
-            currentRow = triangle.get(i - 1);
-            previousRow = currentRow;
-            for (int j = 0; j < i; j++) {
-                if (j == 0) { currentRow.add(j, 1);}
-                else if (j == currentRow.size() - 1) { currentRow.add(j, 1);}
-                else {
-                    currentRow.add(previousRow.get(j - 1) + previousRow.get(j));
+        List<List<Integer>> triangle = new ArrayList<>(numRows);
+        for (int row = 0; row < numRows; row++) {
+            List<Integer> currentRow = new ArrayList<>(row + 1);
+            for (int position = 0; position <= row; position++) {
+                if (position == 0 || position == row) {
+                    currentRow.add(1);
+                } else {
+                    currentRow.add(triangle.get(row - 1).get(position - 1) + triangle.get(row - 1).get(position));
                 }
             }
-            previousRow = triangle.get(i);
+            triangle.add(currentRow);
         }
         return triangle;
 
